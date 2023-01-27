32d0846b115d5e702e1608e098b2236be5509784
LAPTOP-L99LBQ0Q\Gendis <gendispragitaputri@gmail.com>
2023-01-27 12:33:46 +0700

    about-us done

---
+++ application/views/page/about-us.php
@@ -0,0 +1,23 @@
+<section class="bg-light" id="#">
+  <div class="container px-5">
+    <h2 class="display-1 lh-1 mb-3 text-black">About Us Sociolla</h2>
+    <img src="<?= base_url('assets/sociolla/'); ?>assets/img/aboutus.webp" width="90%" />
+    <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center mt-3">
+      <div class="col-3 me-5 text-start align-items-center justify-content-center">
+
+          <p class="lead fs-6 fw-bold mb-0 text-black">YOUR ONLINE BEAUTY DESTINATION</p>
+          <p class="lead fs-6 fw-normal mb-0 text-black">Sociolla is Indonesia's trusted and most complete online shopping destination offering authentic beauty products e.g. make up, skin care, hair care, fragrance and beauty tools serving women across Indonesia.
+          </p>
+      </div>
+      <div class="col-9 me-5 text-start align-items-center justify-content-center">
+        <p class="lead fs-6 fw-bold mb-0 text-black">ONLY THE AUTHENTIC AND BPOM CERTIFIED
+        <p class="lead fs-6 fw-normal mb-0 text-black">Quality and authenticity of beauty product is akin to the importance of taking care of your own skin, a necessity to all women and Sociolla takes it very seriously. Usage of non-original or fraudulent beauty products could lead to permanent skin damage.
+
+          This has prompted Sociolla to collaborate directly with authorized national distributors and brand owners in Indonesia to ensure that every product you purchase from Sociolla are original and authentic, with the certification from Badan Pengawas Obat dan Makanan (‘BPOM’).
+
+          Quality assurance of product storage is performed directly by Sociolla's own operations management with compliance to international standards and storage guideline by the brand owners.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
